fa888989051a723fd5eaff31ccc33fe893e186f4
yankibtw <143903957+yankibtw@users.noreply.github.com>
2024-05-01 14:47:37 +0300

    all 5 rules

--- typograph/typograph/Typograph.cs
+++ typograph/typograph/Typograph.cs
@@ -19,31 +19,49 @@ namespace typograph
         private void TypographyText(string inputText)
         {
             if (!string.IsNullOrEmpty(inputText)) {
-
-                ///1st rule
-                string result = Regex.Replace(inputText, @"(\p{L})\s+(\p{P})", "$1$2");
-                result = Regex.Replace(result, @"([^\w\s]+)", "$0 ");
-                result = Regex.Replace(result, @"(\w)(-)(\w)", "$1 $2 $3");
-                result = Regex.Replace(result, @"([({<«])\s*(\w)", "$1$2");
-                result = Regex.Replace(result, @"(\w)([)}\]>»])(\w)", "$1$2 $3");
-                result = Regex.Replace(result, @"([""“])\s*(\w)", " $1$2");
-                result = Regex.Replace(result, @"(\w)([""”])(\w)", "$1$2 $3");
-
-                ///2nd rule
-                result = Regex.Replace(result, @"\s{2,}", " ");
-
-                ///3rd rule
-                result = Regex.Replace(result, "\"(.*?)\"", match =>
-                {
-                    string captured = match.Groups[1].Value;
-                    string firstWord = char.ToUpper(captured[0]) + captured.Substring(1);
-                    return "«" + firstWord + "»";
-                });
-
-
-
-                mainTextBox.Text = result;
+                mainTextBox.Text = ChangePunctuation(inputText);
             }
+        }
+
+        //rule 1
+        private string ChangePunctuation(string input)
+        {
+            string result = Regex.Replace(input, @"(\p{L})\s+(\p{P})", "$1$2");
+            result = Regex.Replace(result, @"([^\w\s]+)", "$0 ");
+            result = Regex.Replace(result, @"(\w)(-)(\w)", "$1 $2 $3");
+            result = Regex.Replace(result, @"([({<«])\s*(\w)", "$1$2");
+            result = Regex.Replace(result, @"(\w)([)}\]>»])(\w)", "$1$2 $3");
+            result = Regex.Replace(result, @"([""“])\s*(\w)", " $1$2");
+            result = Regex.Replace(result, @"(\w)([""”])(\w)", "$1$2 $3");
+
+            return WhitespChangingTheSpellingOfSpaceace(result);
+        }
+        //rule 2
+        private string WhitespChangingTheSpellingOfSpaceace(string input)
+        {
+            return ChangingQuotes(Regex.Replace(input, @"\s{2,}", " "));
+        }
+        //rule 3
+        private string ChangingQuotes(string input)
+        {
+            string result =  Regex.Replace(input, "\"(.*?)\"", match =>
+            {
+                string captured = match.Groups[1].Value;
+                string firstWord = char.ToUpper(captured[0]) + captured.Substring(1);
+                return "«" + firstWord + "»";
+            });
+
+            return ReplacingTheSignApproximately(result);
+        }
+        //rule 9
+        private string ReplacingTheSignApproximately(string input)
+        {
+            return ReplaceEllipsis(Regex.Replace(input, @"\+\-|\-\+", " ± "));
+        }
+        //rule 13
+        private string ReplaceEllipsis(string input)
+        {
+            return input.Replace("...", "…");
         }
     }
 }
